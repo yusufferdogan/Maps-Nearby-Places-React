d1b793073d3bdbdc5873266795873c9b1b83cce6
yusufferdogan <yusuffx03@gmail.com>
2024-06-30 23:47:26 +0300

    feat: Add lazy loading for images in PlacesMap component

--- src/components/PlacesMap.js
+++ src/components/PlacesMap.js
@@ -1,36 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 import {
   APIProvider,
   Map,
   Marker,
+  InfoWindow
 } from "@vis.gl/react-google-maps";
 import { useSelector } from "react-redux";
 
 const PlacesMap = () => {
-  const { places } = useSelector((state) => state.places || { places: [] });
+  const { places } = useSelector(
+    (state) => state.places || { places: [] }
+  );
   const { center } = useSelector((state) => state.center);
   const apiKey = process.env.REACT_APP_API_KEY;
+  const [selectedPlace, setSelectedPlace] = useState(null);
 
-  const locations = places?.data?.places.map((place) => ({
-    key: place.placeId,
-    location: {
-      lat: place.latitude,
-      lng: place.longitude,
-    },
-  })) || [];
-  console.log(places)
-  console.log(locations)
-  console.log(center)
-  console.log('API Key:', apiKey);
+  const handleMarkerClick = useCallback((place) => {
+    setSelectedPlace(place);
+  }, []);
+
+  console.log("places.",places);
+  console.log("center", center);
+  console.log("selectedPlace:", selectedPlace);
 
   return (
-    <APIProvider
-      apiKey={apiKey}
-    >
+    <APIProvider apiKey={apiKey}>
       <Map
         defaultZoom={13}
         defaultCenter={{ lat: 40.9781815, lng: 29.075794 }}
-        center = {center}
+        center={center}
         initialViewState={{
           latitude: 40.9905539,
           longitude: 29.0195239,
@@ -38,21 +36,32 @@ const PlacesMap = () => {
         }}
         controller={true}
         style={{ width: "100%", height: "700px" }}
-        
       >
-      {locations.map((place) => (
+        {places.data.places.map((place) => (
           <Marker
-            key={place.key}
-            position={place.location}
+            key={place.placeId}
+            position={{lat: place.latitude, lng: place.longitude}}
+            onClick={() => handleMarkerClick(place)}
           />
-        ))
-      }
-
-
+        ))}
+        {selectedPlace && (
+          <InfoWindow
+            position={{
+              lat: selectedPlace.latitude,
+              lng: selectedPlace.longitude,
+            }}
+            onCloseClick={() => setSelectedPlace(null)}
+          >
+            <div>
+              <h2>{selectedPlace.name}</h2>
+              <p>{selectedPlace.vicinity}</p>
+              <p>Rating: {selectedPlace.rating}</p>
+            </div>
+          </InfoWindow>
+        )}
       </Map>
     </APIProvider>
   );
 };
-
 
 export default PlacesMap;
