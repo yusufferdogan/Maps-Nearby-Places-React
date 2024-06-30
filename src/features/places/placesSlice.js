38a896a2aba289d28ad55bd86c0ecae4074e8328
yusufferdogan <yusuffx03@gmail.com>
2024-06-30 21:38:26 +0300

    create place slicer

---
+++ src/features/places/placesSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchPlaces = createAsyncThunk(
+  'places/fetchPlaces',
+  async ({ latitude, longitude, radius }) => {
+    console.log("lat",latitude, "long", longitude, "radius", radius);
+    const response = await axios.get(
+      // `https://cors-anywhere.herokuapp.com/https://web-production-8958.up.railway.app/api/places/search`,
+      'api/places/search',
+      { params: { latitude, longitude, radius } }
+    );
+    return response.data;
+  }
+);
+
+const placesSlice = createSlice({
+  name: 'places',
+  initialState: {
+    places: [],
+    status: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPlaces.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchPlaces.fulfilled, (state, action) => {
+        state.places = action.payload;
+        state.status = 'succeeded';
+      })
+      .addCase(fetchPlaces.rejected, (state) => {
+        state.status = 'failed';
+      });
+  },
+});
+
+export default placesSlice.reducer;
